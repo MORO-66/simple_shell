47dc5a1c3f27d21290acca505d9fe8ea06ff4edd
MORO-66 <mohamedragab987321654@gmail.com>
2023-10-17 19:19:26 +0300

    sdf

--- removeComments.c
+++ removeComments.c
@@ -1,17 +1,19 @@
 #include "headers.h"
-/*
-*remove_comments - function replaces first instance of '#' with '0'
-*@buf address of the string to modify
-* Return Always 0;
-*/
-void remove_comments(char buf)
+
+/**
+ * remove_comments - function replaces first instance of '#' with '\0'
+ * @buf: address of the string to modify
+ *
+ * Return: Always 0;
+ */
+void remove_comments(char *buf)
 {
 	int i;
 
-	for (i = 0; buf[i] != '0'; i++)
-		if (buf[i] == '#' && (!i  buf[i - 1] == ' '))
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
-			buf[i] = '0';
+			buf[i] = '\0';
 			break;
 		}
 }
