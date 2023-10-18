58d7bc3e3e9c6747cb01c8e33e747aa8d39f9ae7
MORO-66 <mohamedragab987321654@gmail.com>
2023-10-18 14:30:10 -0700

    fix

---
+++ cm.c
@@ -0,0 +1,23 @@
+#include "headers.h"
+/**
+ * cmd_sure - determines if a file is an executable command
+ * @info: the info struct
+ * @path: path to the file
+ *
+ * Return: 1 if true, 0 otherwise
+ */
+int cmd_sure(info_t *info, char *path)
+{
+	struct stat st;
+
+	(void)info;
+	if (!path || stat(path, &st))
+		return (0);
+
+	if (st.st_mode & S_IFREG)
+	{
+		return (1);
+	}
+	return (0);
+}
+
